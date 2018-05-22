e08871d423d4c905b6d8f792a66c909eb101bc54
joe <anjiao-1314>
2018-05-22 16:03:45 +0800

    初始化

--- app/src/main/java/com/joe/animation/MainActivity.java
+++ app/src/main/java/com/joe/animation/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-
         findViewById(R.id.btnFrameAni).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
